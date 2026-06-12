088787e43cecec33bdfee934b1aab119a267031e
lijs17 <lijs17@asiainfo.com>
2026-06-12 17:13:07 +0800

    Revert "补充初始化"
    This reverts commit c44bcec65155ff98c27acb78dc1273dc540b88ea.

--- backend/src/main/java/com/dcits/bank/demo/backend/service/AccountService.java
+++ backend/src/main/java/com/dcits/bank/demo/backend/service/AccountService.java
@@ -46,7 +46,6 @@ public class AccountService {
         this.transactionMapper = transactionMapper;
         this.cashTransactionMapper = cashTransactionMapper;
         this.accountingService = accountingService;
-        this.interestService = interestService;
     }
 
     //  功能1：客户开户
